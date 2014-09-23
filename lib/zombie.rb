e2125c754edb02cb5a7e2691dcdbdc0684a1ca0b
Jonathan <jonathan.yan22@gmail.com>
2014-09-23 20:39:55 +1000

    jonathan/shannon:random zombie move

--- lib/zombie.rb
+++ lib/zombie.rb
@@ -1,7 +1,25 @@
 require "object"
 
 class Zombie < Object
+  attr_accessor :x, :y
+
+  def initialize(x,y)
+  	@x = x
+  	@y = y
+  end
+
   def say
     "braaaaaaaaaaaiiiiinnnnnnnnnns"
+  end
+
+  def move
+  	original_x = x
+  	original_y = y
+  	
+  	loop do
+  		@x = @x + Random.new.rand(-1..1)
+  		@y = @y + Random.new.rand(-1..1)
+  		break if @x != original_x or @y != original_y
+  	end
   end
 end
--- spec/zombie_spec.rb
+++ spec/zombie_spec.rb
@@ -2,8 +2,24 @@ require 'rspec'
 require 'zombie'
 
 describe Zombie do
+	let(:zombie) {Zombie.new(10,10)}
+
   it "should moan about brains" do
-    d = Zombie.new
-    expect(d.say).to eq('braaaaaaaaaaaiiiiinnnnnnnnnns')
+    expect(zombie.say).to eq('braaaaaaaaaaaiiiiinnnnnnnnnns')
   end
+
+  it "should change position when moved" do 
+  	zombie.move
+  	expect([zombie.x, zombie.y]).not_to eq([10,10])
+  end
+
+  it "should move one position" do
+  	zombie.move
+  	expect(zombie.x).to be <= 11
+  	expect(zombie.x).to be >= 9
+  	expect(zombie.y).to be <= 11
+  	expect(zombie.y).to be >= 9
+  	
+  end
+
 end
