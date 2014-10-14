863432eaef1c903f4e3df06a82daf9275c3d74ac
Roger Lo <farfromnear@gmail.com>
2014-10-14 10:20:06 +0000

    Added a few test for direction. Finished the gen_direction method for zombie

--- spec/zombie_spec.rb
+++ spec/zombie_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 require 'zombie'
+require 'direction'
 
 describe Zombie do
+	let(:zombie) {Zombie.new}
+	
 	it "should moan about brains" do
-		z = Zombie.new
-    expect(z.say).to eq('braaaaaaaaaaaiiiiinnnnnnnnnns')
+    expect(zombie.say).to eq('braaaaaaaaaaaiiiiinnnnnnnnnns')
+	end
+
+	it "should return random direction" do
+
+		expect(Direction.all_directions).to include(zombie.gen_direction)
 	end
 end
