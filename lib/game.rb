51c87099fda6c06fe8ede522fcbde4baff206d89
Alex Tan <alext@outlook.com.au>
2014-10-14 20:41:55 +1100

    addded game class

---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+class Game
+	def intialize
+	end
+
+	def start_game
+		human = Human.new
+		board = Board.new(5)
+		humanStartPoint = Point.new(0,0)
+		board.put(humanStartPoint, human)
+		
+		humanPos = board.find(human)
+		puts "Human position is ( #{humanPos.x} , #{humanPos.y} )"
+	end
+
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'game'
+
+describe Game do
+	it 'should output human position'
+   g = Game.new
+	g.start_game
+end
